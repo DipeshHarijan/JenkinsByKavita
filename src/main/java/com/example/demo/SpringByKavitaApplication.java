7f8929b41aa77f278a2a8246c4fa59e3bf1aef73
DipeshHarijan <dipeshharijan1122@gmail.com>
2020-02-19 15:29:44 +0530

    changing main method

--- src/main/java/com/example/demo/SpringByKavitaApplication.java
+++ src/main/java/com/example/demo/SpringByKavitaApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringByKavitaApplication {
+public class SpringByKavitaApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringByKavitaApplication.class, args);
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringByKavitaApplication.class);
+	}
 }
